d2e47f3e4e4e1d3b1ec55b3ea6e4c0c2de25f2c4
Gouriwara Kumara <44789848+Arjuna7@users.noreply.github.com>
2021-02-04 03:28:41 +0530

    Update Zero.sh

--- Zero.sh
+++ Zero.sh
@@ -1,1 +1,22 @@
 
+
+# Arch Linux installation
+
+
+
+lsblk
+echo "Give Uefi and Root disk"
+echo "Please enter disk: (example /dev/sda)"
+read DISK
+lsblk
+echo "Give Swap and Home disk"
+echo "Please enter disk: (example /dev/sdb)"
+read Disk
+#---------------------------------------------------------------------------------
+
+echo "--------------------------------------------------"
+echo "-----------Update the system clock----------------"
+echo "--------------------------------------------------"
+
+timedatectl set-ntp true
+timedatectl status
