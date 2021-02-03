a184339ce783d10e360221296d585895936f1c1b
Gouriwara Kumara <44789848+Arjuna7@users.noreply.github.com>
2021-02-04 03:25:49 +0530

    Update preinstall.sh

--- preinstall.sh
+++ preinstall.sh
@@ -10,27 +10,6 @@ lsblk
 echo "Give Swap and Home disk"
 echo "Please enter disk: (example /dev/sdb)"
 read Disk
-
-#-------------------------------------------------------------------------------
-
-if ! source install.conf; then
-	read -p "Please enter hostname:" hostname
-
-	read -p "Please enter username:" username
-
-	read -sp "Please enter password:" password
-
-	read -sp "Please repeat password:" password2
-
-	# Check both passwords match
-	if [ "$password" != "$password2" ]; then
-	    echo "Passwords do not match"
-	    exit 1
-	fi
-  printf "hostname="$hostname"\n" >> "install.conf"
-  printf "username="$username"\n" >> "install.conf"
-  printf "password="$password"\n" >> "install.conf"
-fi
 #---------------------------------------------------------------------------------
 
 echo "--------------------------------------------------"
