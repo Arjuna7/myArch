cd07ef80014d80f9fe53e5e4e2df040b83173c84
Gouriwara Kumara <44789848+Arjuna7@users.noreply.github.com>
2021-02-04 11:22:03 +0530

    Create 2confchroot.sh

---
+++ 2confchroot.sh
@@ -0,0 +1,11 @@
+#----------------------------------------------------------------------------------
+echo "-------------------------------------------------"
+echo "       Setup Language to US and set locale       "
+echo "-------------------------------------------------"
+sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
+locale-gen
+# ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
+timedatectl --no-ask-password set-timezone Asia/Kolkata
+timedatectl --no-ask-password set-ntp 1
+localectl --no-ask-password set-locale LANG="en_US.UTF-8" LC_COLLATE="" LC_TIME="en_US.UTF-8"
+#---------------------------------------------------------
