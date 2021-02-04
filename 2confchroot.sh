616a6308476a0acfbeec4eb03d76979e18234afb
Gouriwara Kumara <44789848+Arjuna7@users.noreply.github.com>
2021-02-04 11:30:07 +0530

    Update 2confchroot.sh

--- 2confchroot.sh
+++ 2confchroot.sh
@@ -9,3 +9,32 @@ timedatectl --no-ask-password set-timezone Asia/Kolkata
 timedatectl --no-ask-password set-ntp 1
 localectl --no-ask-password set-locale LANG="en_US.UTF-8" LC_COLLATE="" LC_TIME="en_US.UTF-8"
 #---------------------------------------------------------
+
+
+#---------------------------------------------------------------------------------------------------------------------------
+mkinitcpio -p linux
+mkinitcpio -p linux-lts
+#---------------------------------------
+
+# Set keymaps
+localectl --no-ask-password set-keymap us
+
+# Hostname
+hostnamectl --no-ask-password set-hostname Garuda
+#--------------------------------------
+#-------------------------------------------------------------------------------------
+echo "--------------------------------------"
+echo "--      Set Password for Root       --"
+echo "--------------------------------------"
+echo "Enter password for root user: "
+passwd 
+
+useradd -m -g users -G wheel lucifer
+passwd lucifer
+
+# Add sudo rights
+sed -i 's/^# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
+
+
+systemctl enable sshd
+systemctl enable NetworkManager
