fec21c941605a7871a3ec7a342567c79c46fc2b2
Gouriwara Kumara <44789848+Arjuna7@users.noreply.github.com>
2021-02-04 10:13:25 +0530

    Update 1arch-chrootpkg.sh

--- 1arch-chrootpkg.sh
+++ 1arch-chrootpkg.sh
@@ -2,9 +2,13 @@
 echo "--------------------------------------"
 echo "--          linux package           --"
 echo "--------------------------------------"
-pacman -S linux linux-headers linux-lts linux-lts-headers
+pacman -S linux linux-headers linux-lts linux-lts-headers --noconfirm --needed
 
 echo "--------------------------------------"
 echo "--  text editor,othet packages      --"
 echo "--------------------------------------"
 pacman -S vim openssh base-devel nano sudo dialog --noconfirm --needed
+echo "--------------------------------------"
+echo "--          Network Setup           --"
+echo "--------------------------------------"
+pacman -S networkmanager wpa_supplicant wireless_tools netctl --noconfirm --needed
